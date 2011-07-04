d84a591e5d0a81b8f98c2e49ad93d84bfec20e2a
ntucker <none@none>
2011-07-04 13:02:19 -0700

    Make rendering with a tag queryset possible.

--- taggit/forms.py
+++ taggit/forms.py
@@ -7,7 +7,7 @@ from taggit.utils import parse_tags, edit_string_for_tags
 class TagWidget(forms.TextInput):
     def render(self, name, value, attrs=None):
         if value is not None and not isinstance(value, basestring):
-            value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
+            value = edit_string_for_tags(value)
         return super(TagWidget, self).render(name, value, attrs)
 
 class TagField(forms.CharField):
